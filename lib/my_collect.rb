25f217250997a72e3d53ce878ba8ffb4d52c0e68
Santiago Sendon <santiagosendon92@gmail.com>
2018-08-19 21:59:42 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,13 @@
+def my_collect(array) # put argument(s) here
+  # code here
+  i = 0
+  result = []
 
+  while i < array.size
+    block_given? 
+    result << yield(array[i]) 
+    i += 1
+  end
+result
+end
 
